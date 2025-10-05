6a5811a6ec75b0f9c2b3ff531c0f8a89fde8dad5
Team Merkmakers <info@merkmakers.net>
2025-10-05 22:48:03 +0000

    use the icon of the prefious zero state

--- src/components/login-screen.tsx
+++ src/components/login-screen.tsx
@@ -92,6 +92,17 @@ export default function LoginScreen({ profiles, onAccountCreate, onLogin, onProf
     }
   }
 
+  const AddProfileCard = () => (
+    <ProfileCard onClick={openDialog}>
+      <div className="p-2">
+        <Avatar className="flex h-20 w-20 items-center justify-center bg-muted/50">
+            <UserPlus className="h-8 w-auto text-muted-foreground/50" strokeWidth={1}/>
+        </Avatar>
+      </div>
+      <Button size="xs" variant="outline" className='mt-2'>+ New Profile</Button>
+    </ProfileCard>
+  );
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center animate-in fade-in duration-500">
       <div className="flex flex-row items-start gap-8 p-8">
@@ -112,14 +123,7 @@ export default function LoginScreen({ profiles, onAccountCreate, onLogin, onProf
               </Button>
           </ProfileCard>
         ))}
-        <ProfileCard onClick={openDialog}>
-            <div className="p-2">
-                <Avatar className="flex h-20 w-20 items-center justify-center bg-muted/50">
-                    <UserPlus className="h-8 w-auto text-muted-foreground/50" strokeWidth={1}/>
-                </Avatar>
-            </div>
-            <Button size="xs" variant="outline" className='mt-2'>+ New Profile</Button>
-        </ProfileCard>
+        <AddProfileCard />
       </div>
 
       <Dialog open={isOpen} onOpenChange={onDialogStateChange}>
