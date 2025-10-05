409bb3926d6be2ba1bb52584802d2c1aa11597fd
Team Merkmakers <info@merkmakers.net>
2025-10-05 22:54:05 +0000

    the width of the profile card before you click on new proflile is now al

--- src/components/login-screen.tsx
+++ src/components/login-screen.tsx
@@ -76,7 +76,7 @@ export default function LoginScreen({ profiles, onAccountCreate, onLogin, onProf
   };
 
   const AddProfileCard = () => (
-    <ProfileCard onClick={!isCreating ? openCreator : undefined} className="w-[200px]">
+    <ProfileCard onClick={!isCreating ? openCreator : undefined} className={isCreating ? "w-[200px]" : "w-36"}>
       <div className="p-2">
         <div className="flex h-20 w-20 items-center justify-center rounded-full bg-muted/50">
           <UserCircle className="h-8 w-auto text-muted-foreground/50" strokeWidth={1} />
