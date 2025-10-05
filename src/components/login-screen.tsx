3c3d9a3f4a08bc72176bf09724cecfed5d620918
Team Merkmakers <info@merkmakers.net>
2025-10-05 22:32:02 +0000

    add 4px padding to the profile compentn, so the hover nackground isnt to

--- src/components/login-screen.tsx
+++ src/components/login-screen.tsx
@@ -87,7 +87,7 @@ export default function LoginScreen({ onAccountCreate, onLogin, hasProfile, prof
       <div className="flex flex-row items-start gap-8">
         {hasProfile && profile ? (
           <>
-            <Button variant="ghost" className="h-auto w-auto p-0 flex flex-col gap-2 text-center" onClick={onLogin}>
+            <Button variant="ghost" className="h-auto w-auto p-1 flex flex-col gap-2 text-center" onClick={onLogin}>
                 <Avatar className="h-20 w-20">
                     <AvatarImage src={profile.avatarUrl} alt={profile.username} />
                     <AvatarFallback>{profile.username.charAt(0)}</AvatarFallback>
@@ -97,7 +97,7 @@ export default function LoginScreen({ onAccountCreate, onLogin, hasProfile, prof
             
             <div className="flex flex-col gap-4 items-center">
               <Avatar className="h-20 w-20 flex items-center justify-center bg-muted/50 cursor-pointer" onClick={openDialog}>
-                  <UserPlus className="h-10 w-10 text-muted-foreground/50" strokeWidth={1}/>
+                  <UserPlus className="h-8 w-8 text-muted-foreground/50" strokeWidth={1}/>
               </Avatar>
               <Button size="xs" variant="outline" onClick={openDialog}>+ New Profile</Button>
             </div>
