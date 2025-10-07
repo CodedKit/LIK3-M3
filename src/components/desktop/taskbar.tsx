314697651ff50ca60542a5f70a4fa279fb92c8ed
Team Merkmakers <info@merkmakers.net>
2025-10-07 01:46:55 +0000

    change the avatar to be a square with a 4px border radiius

--- src/components/desktop/taskbar.tsx
+++ src/components/desktop/taskbar.tsx
@@ -35,7 +35,7 @@ export default function Taskbar({ userProfile, onLogout, onReset, onOpenSettings
                                     alt={userProfile.username}
                                     width={28}
                                     height={28}
-                                    className="rounded-full"
+                                    className="rounded-sm"
                                 />
                             )}
                         </button>
