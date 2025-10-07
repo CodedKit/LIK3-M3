823b6741dc026688036b45eb90e5db54ec1d287c
Team Merkmakers <info@merkmakers.net>
2025-10-07 01:47:26 +0000

    make it a perfect s 1x1 sqaure

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -12,7 +13,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-md",
+      "relative flex shrink-0 overflow-hidden rounded-md",
       className
     )}
     {...props}
