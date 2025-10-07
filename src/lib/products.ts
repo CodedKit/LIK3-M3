8b35fb056351634d9b5310d5f2bd42aecd5fb7af
Team Merkmakers <info@merkmakers.net>
2025-10-07 03:50:28 +0000

    1.create a seperate product json's for each of the products, this makes

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,6 +1,9 @@
 
-import data from './products.json';
 import { PlaceHolderImages, type ImagePlaceholder } from './placeholder-images';
+import chronoSmartwatch from './products/chrono-smartwatch.json';
+import noiseAwayHeadphones from './products/noise-away-headphones.json';
+import skyHighDrone from './products/sky-high-drone.json';
+import virtusphereVr from './products/virtusphere-vr.json';
 
 export type Product = {
   id: string;
@@ -10,7 +13,14 @@ export type Product = {
   image: ImagePlaceholder;
 };
 
-export const Products: Product[] = data.products.map(product => {
+const productData = [
+    chronoSmartwatch,
+    noiseAwayHeadphones,
+    skyHighDrone,
+    virtusphereVr,
+];
+
+export const Products: Product[] = productData.map(product => {
     const image = PlaceHolderImages.find(img => img.id === product.imageId);
     if (!image) {
         throw new Error(`Could not find image with id ${product.imageId}`);
