271cf5059091e5ee6bc6e2a0a417127aefc5167c
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-10-28 02:37:41 +0300

    Update testChatApp.tsx

--- src/components/desktop/apps/testChatApp.tsx
+++ src/components/desktop/apps/testChatApp.tsx
@@ -1,7 +1,15 @@
+import { StoryPlayer } from '@/components/story/StoryPlayer'
+import { StoryProvider } from '@/context/StoryContext'
 import React from 'react'
 
 export const TestChatApp = () => {
+    const storyToLoad = '/stories/my_story.json';
+
     return (
-        <div>testChatApp</div>
+        <main>
+            <StoryProvider storyJsonPath={storyToLoad}>
+                <StoryPlayer />
+            </StoryProvider>
+        </main>
     )
 }
